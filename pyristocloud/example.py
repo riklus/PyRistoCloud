ad2b56a8184548e61ef85b7e39592e8a4c29bf59
riklus <lussana.riccardo@gmail.com>
2021-12-29 10:08:27 +0100

    template commit

---
+++ pyristocloud/example.py
@@ -0,0 +1,19 @@
+"""Example of code."""
+
+
+def hello(name: str) -> str:
+    """Just an greetings example.
+
+    Args:
+        name (str): Name to greet.
+
+    Returns:
+        str: greeting message
+
+    Examples:
+        .. code:: python
+
+            >>> hello("Riklus")
+            'Hello Riklus!'
+    """
+    return f"Hello {name}!"
